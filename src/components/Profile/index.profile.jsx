aadd10055f7162573db79d1a6f95fa5f1151d5f6
Muukit <51193781+Muukit@users.noreply.github.com>
2021-07-08 18:43:05 +0600

    Update index.profile.jsx

---
+++ src/components/Profile/index.profile.jsx
@@ -0,0 +1,14 @@
+import React , {Component} from 'react'
+
+class Profile extends Component (){
+  render (){
+    return (
+      <div>
+        <div className="Bio">
+            <h2> Abdul Mukit Al Mehedy</h2>  
+            <p> Junior Frontend Developer</p>
+        </div>
+      </div>
+    )
+  }
+}
