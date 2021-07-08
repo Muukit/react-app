c7bd82118fe8f7e10a5e1cd950698ce2b9f7dc84
Muukit <51193781+Muukit@users.noreply.github.com>
2021-07-08 20:20:19 +0600

    Update index.profile.jsx

--- src/components/Profile/index.profile.jsx
+++ src/components/Profile/index.profile.jsx
@@ -4,10 +4,27 @@ class Profile extends Component (){
   render (){
     return (
       <div>
-        <div className="Bio">
-            <h2> Abdul Mukit Al Mehedy</h2>  
-            <p> Junior Frontend Developer</p>
-        </div>
+     <div className="container"> 
+   <div className="Bio"> 
+    <h3>Abdul Mukit Al Mehedy</h3> 
+    <p>Junior Frontend Developer</p> 
+   </div> 
+   <div className="Skills"> 
+    <h4>Skills</h4> 
+    <ul> 
+     <li>Python</li> 
+     <li>Javascript</li> 
+    </ul> 
+   </div> 
+   <div className="Links"> 
+    <h4>Social Link</h4> 
+    <ul> 
+     <li><a href="#">Facebook</a></li> 
+     <li><a href="#">Instagram</a></li> 
+     <li><a href="#">Github</a></li> 
+    </ul> 
+   </div> 
+  </div>
       </div>
     )
   }
